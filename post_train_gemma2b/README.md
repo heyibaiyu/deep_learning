896142701f0f1718f5341adf0b7cd662d509fa69
Jing <lijingwoshilijing@126.com>
2025-12-02 11:58:46 -0800

    DPO fine-tune on gemma-2b

---
+++ post_train_gemma2b/README.md
@@ -0,0 +1,8 @@
+## Post training of model google/gemma-2b-it
+### Dataset
+* Use UltraFeedback dataset: general-purpose preference data; diverse topic
+https://huggingface.co/datasets/HuggingFaceH4/ultrafeedback_binarized/viewer/default/train_prefs?views%5B%5D=train_prefs
+* size: 61K
+
+### Base Model
+google/gemma-2b-it
---
+++ post_train_gemma2b/data_util.py
@@ -0,0 +1,20 @@
+from datasets import load_dataset, Dataset
+
+
+def load_data_ultra_feedback() -> Dataset:
+    ds = load_dataset("HuggingFaceH4/ultrafeedback_binarized",split='train_prefs')
+    print(ds)
+    prompts = []
+    for row in ds:
+        prompt = row['prompt']
+        chosen = row['chosen'][1]['content']
+        rejected = row['rejected'][1]['content']
+        prompts.append({'prompt': prompt, 'chosen': chosen, 'rejected': rejected})
+    return Dataset.from_list(prompts)
+
+ds = load_data_ultra_feedback()
+print(ds[0])
+# print('------------------------')
+# print(ds[0]['rejected'])
+
+
